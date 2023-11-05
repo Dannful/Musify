8732f2c9e04b9b4dd1ef7f3296c04b85a98bce55
Vinícius Daniel <dannful@gmail.com>
2023-11-05 14:39:47 -0300

    Controlling new UI elements

--- src/main/java/com/github/musify/presentation/MusifyController.java
+++ src/main/java/com/github/musify/presentation/MusifyController.java
@@ -1,48 +1,164 @@
 package com.github.musify.presentation;
 
+import com.github.musify.domain.model.Instruments;
 import com.github.musify.domain.model.MusicProcesser;
-import com.github.musify.domain.model.Note;
 import com.github.musify.domain.model.PatternBuilder;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.util.Duration;
+import org.jfugue.player.ManagedPlayer;
 import org.jfugue.player.Player;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class MusifyController {
 
     @FXML
-    private TextArea inputText;
+    public Spinner<Integer> bpmSpinner;
+    @FXML
+    public Button stopMusicButton;
+    @FXML
+    public Button loadFileButton;
+    @FXML
+    private RadioButton bpmButton;
 
     @FXML
-    private Button convertButton;
+    public Button convertMusicButton;
 
-    private void playSomethingCool() {
-        Player player = new Player();
-        PatternBuilder builder = new PatternBuilder();
-        builder.addNote(new Note(Note.BaseNote.A, (short) 2));
-        builder.addNote(new Note(Note.BaseNote.E, (short) 4));
-        builder.addNote(new Note(Note.BaseNote.E, (short) 4));
-        builder.addNote(new Note(Note.BaseNote.C, (short) 4));
-        builder.addNote(new Note(Note.BaseNote.B, (short) 3));
-        builder.addNote(new Note(Note.BaseNote.B, (short) 3));
-        builder.addNote(new Note(Note.BaseNote.B, (short) 3));
-        builder.addNote(new Note(Note.BaseNote.B, (short) 3));
-        builder.addNote(new Note(Note.BaseNote.C, (short) 4));
-        builder.addNote(new Note(Note.BaseNote.A, (short) 3));
-        builder.addNote(new Note(Note.BaseNote.E, (short) 4));
-        builder.addNote(new Note(Note.BaseNote.E, (short) 4));
-        builder.addNote(new Note(Note.BaseNote.C, (short) 4));
-        builder.cleanPattern();
-        new Thread(() -> player.play(builder.build()), "Cool music").start();
+    @FXML
+    private MenuBar menu;
+
+    @FXML
+    private TextArea fileField;
+
+    @FXML
+    private Button playMusicButton;
+
+    @FXML
+    private TextArea textFieldForConvert;
+
+    @FXML
+    private ChoiceBox<String> instrumentChoiceField;
+
+    @FXML
+    private ProgressBar convertProcessBar;
+
+    @FXML
+    private Slider musicProgress;
+
+    @FXML
+    private AnchorPane mainArea;
+
+    @FXML
+    private Menu help;
+
+    @FXML
+    private Label labelBpm;
+
+    @FXML
+    private Label labelFile;
+
+    @FXML
+    private Label labelInstruments;
+
+    @FXML
+    private Separator separator;
+
+    private PatternBuilder processedSong;
+    private ManagedPlayer managedPlayer;
+    private Timeline timeline;
+
+    public void initialize() {
+        textFieldForConvert.textProperty().addListener((observable, oldValue, newValue) -> convertMusicButton.setDisable(newValue.isBlank()));
+        SpinnerValueFactory.IntegerSpinnerValueFactory spinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(60, 1000, 60);
+        spinnerValueFactory.setAmountToStepBy(60);
+        bpmSpinner.setValueFactory(spinnerValueFactory);
+    }
+
+    private void lockUI() {
+        textFieldForConvert.setDisable(true);
+        convertMusicButton.setDisable(true);
+        playMusicButton.setDisable(true);
+        bpmSpinner.setDisable(true);
+        instrumentChoiceField.setDisable(true);
+        loadFileButton.setDisable(true);
+    }
+
+    private void unlockUI() {
+        textFieldForConvert.setDisable(false);
+        convertMusicButton.setDisable(false);
+        playMusicButton.setDisable(false);
+        bpmSpinner.setDisable(false);
+        instrumentChoiceField.setDisable(false);
+        stopMusicButton.setDisable(true);
+        loadFileButton.setDisable(false);
     }
 
     @FXML
-    private void onConvertClick() {
-        final String text = inputText.getText();
-        if (text.isBlank())
+    private void onGenerateClicked() {
+        lockUI();
+        final Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() {
+                MusicProcesser processer = new MusicProcesser(textFieldForConvert.getText(), new PatternBuilder(Instruments.valueOf(instrumentChoiceField.getValue().toUpperCase(Locale.ROOT).replaceAll(" ", "_")), bpmSpinner.getValue().shortValue()));
+                processedSong = processer.process((progress) -> updateProgress(progress, textFieldForConvert.getText().length() - 1));
+                unlockUI();
+                return null;
+            }
+        };
+        convertProcessBar.progressProperty().bind(task.progressProperty());
+        Thread taskThread = new Thread(task);
+        taskThread.start();
+    }
+
+    @FXML
+    private void onPlayClicked() {
+        lockUI();
+        stopMusicButton.setDisable(false);
+        Player player = new Player();
+        managedPlayer = player.getManagedPlayer();
+        new Thread(() -> player.play(processedSong.build())).start();
+        timeline = new Timeline();
+        musicProgress.setMax(processedSong.getDuration());
+        timeline.getKeyFrames().add(new KeyFrame(Duration.ZERO, new KeyValue(musicProgress.valueProperty(), 0d)));
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(processedSong.getDuration()), new KeyValue(musicProgress.valueProperty(), processedSong.getDuration())));
+        timeline.play();
+        timeline.setOnFinished(event -> unlockUI());
+    }
+
+    @FXML
+    private void onStopClicked() {
+        managedPlayer.finish();
+        timeline.stop();
+        musicProgress.setValue(0);
+        unlockUI();
+    }
+
+    @FXML
+    private void onLoadFileClicked() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open text file");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        File selected = fileChooser.showOpenDialog(mainArea.getScene().getWindow());
+        if (selected == null)
             return;
-        MusicProcesser processer = new MusicProcesser(text);
-        new Thread(() -> new Player().play(processer.process()), "Main music").start();
-//        new Thread(this::playSomethingCool, "Main music").start(); uncomment this to listen to something cool
+        try {
+            FileInputStream fileInputStream = new FileInputStream(selected);
+            BufferedReader bufferedReader = new BufferedReader(new java.io.InputStreamReader(fileInputStream));
+            textFieldForConvert.setText(bufferedReader.lines().collect(Collectors.joining("\n")));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }

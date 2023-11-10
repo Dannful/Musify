f2a5d6d40bc5954bb8d7e39b8c572b805ec49c53
Vinícius Daniel <dannful@gmail.com>
2023-11-10 12:52:40 -0300

    Managing Spring boot

--- src/main/java/com/github/musify/Main.java
+++ src/main/java/com/github/musify/Main.java
@@ -1,7 +1,12 @@
 package com.github.musify;
+
+import javafx.application.Application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        MusifyApplication.main(args);
+        Application.launch(MusifyApplication.class, args);
     }
 }
